3d4e7fe0f49e9b09d01bcf3f3d43ea42b5c2a021
DeokHyeon Lee <dlejrgus95@naver.com>
2021-01-29 16:55:28 +0900

    [python basic] chapter08_01
    2021.01.29
    Learn Built-in Function

---
+++ chapter08_01.py
@@ -0,0 +1,15 @@
+# Chapter08-1
+# 파이썬 내장(Built-in) 함수
+# 자주 사용하는 함수 위주로 실습
+# 사용하다보면 자연스럽게 숙달
+# str(), int(), tuple(), 형변환 이미 학습
+
+# 절대값
+# abs()
+print(abs(-3))
+
+# all, any : interable 요소 검사(참, 거짓)
+print(all([1, 2, 3])) # and
+print(any([1, 2, 0])) # or
+
+# chr : 아스키 -> 문자, ord 
